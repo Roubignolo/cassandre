85cbee88f5c2295b654ada8ae72627ffe0bf8ebf
Roubignolo <jeromefiron@hotmail.fr>
2020-02-24 11:30:00 +0100

    Add files via upload

---
+++ download.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Script de MàJ du site Cassand.re"
+echo " "
+cp /var/www/html/js /var/www/html/js2
+cp /var/www/html/ccs /var/www/html/ccs2
+cp /var/www/html/medias /var/www/html/medias2
