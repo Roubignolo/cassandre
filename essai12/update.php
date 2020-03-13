88f92844ca5381f272fa0703df4b672b56766e2d
Roubignolo <jeromefiron@hotmail.fr>
2020-03-13 23:23:16 +0100

    Add files via upload

---
+++ essai12/update.php
@@ -0,0 +1,176 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$nom = $actif = $image = "";
+$nom_err = $actif_err = $image_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Get hidden input value
+    $id = $_POST["id"];
+    
+    // Validate nom
+    $input_nom = trim($_POST["nom"]);
+    if(empty($input_nom)){
+        $nom_err = "Merci de renseigner un nom.";
+    } elseif(!filter_var($input_nom, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $nom_err = "Merci d'entrer un nom valide.";
+    } else{
+        $nom = $input_nom;
+    }
+    
+    // Validate actif
+    $input_actif = trim($_POST["actif"]);
+    if(empty($input_actif)){
+        $actif_err = "Merci de dire si l'agence est active.";     
+    } else{
+        $actif = $input_actif;
+    }
+    
+    // Validate image
+    $input_image = trim($_POST["image"]);
+    if(empty($input_image)){
+        $image_err = "Merci de renseigner le nom du logo.";     
+    } elseif(!filter_var($input_nom, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $image_err = "Merci de renseigner un nom de logo valide.";
+    } else{
+        $image = $input_image;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($nom_err) && empty($actif_err) && empty($image_err)){
+        // Prepare an update statement
+        $sql = "UPDATE agences SET nom=?, actif=?, image=? WHERE id=?";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssss", $param_nom, $param_actif, $param_image, $param_id);
+            
+            // Set parameters
+            $param_nom = $nom;
+            $param_actif = $actif;
+            $param_image = $image;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Erreur. Réessayez ultérieurement.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+} else{
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM agences WHERE id = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                $result = mysqli_stmt_get_result($stmt);
+    
+                if(mysqli_num_rows($result) == 1){
+                    /* Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop */
+                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                    
+                    // Retrieve individual field value
+                    $nom = $row["nom"];
+                    $actif = $row["actif"];
+                    $image = $row["image"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Erreur. Merci de réessayer ultérieurement.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+        
+        // Close connection
+        mysqli_close($link);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mise à jour des informations de l'agence</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        .wrapper{
+            width: 500px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header">
+                        <h2>Mise à jour</h2>
+                    </div>
+                    <p>Modifier les informations de l'agence</p>
+					
+			
+                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                        <div class="form-group <?php echo (!empty($nom_err)) ? 'has-error' : ''; ?>">
+                            <label>Nom</label>
+                            <input type="text" name="nom" class="form-control" value="<?php echo $nom; ?>">
+                            <span class="help-block"><?php echo $nom_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($actif_err)) ? 'has-error' : ''; ?>">
+                            <label>Actif</label>
+                            <select name="actif" class="form-control" value="<?php echo $actif; ?>">
+								<option value="Oui">Active</option>
+								<option value="Non">Non-active</option>
+ 							</select>
+						<span class="help-block"><?php echo $actif_err;?></span>
+						</div>
+                        <div class="form-group <?php echo (!empty($image_err)) ? 'has-error' : ''; ?>">
+                            <label>Logo</label>
+                            <input type="text" name="image" class="form-control" value="<?php echo $image; ?>">
+                            <span class="help-block"><?php echo $image_err;?></span>
+                        </div>
+						<input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                        <input type="submit" class="btn btn-primary" value="Valider">
+                        <a href="index.php" class="btn btn-default">Annuler</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
