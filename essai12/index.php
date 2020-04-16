df7fe4ee39a584515d5401b6cc1f0b608de6c2b8
Roubignolo <jeromefiron@hotmail.fr>
2020-04-16 15:44:18 +0200

    Update index.php

--- essai12/index.php
+++ essai12/index.php
@@ -30,7 +30,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header clearfix">
-                        <h2 class="pull-left">Agences</h2>
+                        <h2 class="pull-left">Agences totototototo</h2>
                         <a href="create.php" class="btn btn-success pull-right">Ajouter une nouvelle agence</a>
                     </div>
                     <?php
